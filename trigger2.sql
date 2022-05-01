b8446ed1a75f4e43cfda967ec04e530ceddc2204
Tit A. Poplatnik <33201077+tpltnk@users.noreply.github.com>
2022-05-01 21:39:10 +0200

    add: trigger example 2

---
+++ trigger2.sql
@@ -0,0 +1,22 @@
+/*
+Pri brisanju tekmovalca, naj se samodejno v tabelo ‘Izbrisani’ vpišejo naslednji podatki: TID, Ime, Priimek, ime uporabnika, ki je opravil brisanje, datum in čas brisanja.
+*/
+
+CREATE TABLE Izbrisani (
+  TID INT NOT NULL,
+  Ime VARCHAR(20) NOT NULL,
+  Priimek VARCHAR(30) NOT NULL,
+  ImeUporabnika VARCHAR(30) NOT NULL
+  DatumCasBrisanja DATETIME NOT NULL,
+  PRIMARY KEY (TID)
+);
+
+CREATE TRIGGER OnDeleteTekmovalec
+FOR Tekmovalec
+ACTIVE AFTER DELETE
+AS
+BEGIN
+  INSERT INTO Izbrisani (TID, Ime, Priimek, ImeUporabnika, DatumCasBrisanja)
+         VALUES (OLD.TID, OLD.Ime, OLD.Priimek, CURRENT_USER, CURRENT_DATETIME);
+END
+
